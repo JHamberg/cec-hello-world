2cbbec9c7e793759389a905cfd7f5df674e77f8e
Jonatan Hamberg <jonatan.hamberg@cs.helsinki.fi>
2018-02-19 16:35:32 +0200

    Use spaces for indentation

--- wsgi.py
+++ wsgi.py
@@ -7,11 +7,11 @@ log_path = "/mnt/log"
 @application.route("/")
 def hello():
     with open(log_path) as f:
-	data = f.readLines()
+        data = f.readLines()
     return data
 
 if __name__ == "__main__":
     application.run()
     with open(log_path, "w") as f:
-	f.write(socket.gethostname() + " " + time.time())
+        f.write(socket.gethostname() + " " + time.time())
 
