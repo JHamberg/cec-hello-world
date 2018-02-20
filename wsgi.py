d3a9b8a01d5a232b66c1d49ce96ac491390dc2a2
Jonatan Hamberg <jonatan.hamberg@cs.helsinki.fi>
2018-02-20 16:05:40 +0200

    Write to log on each access

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 import socket
-from time import time
+from time import ctime
 from flask import Flask
 
 application = Flask(__name__)
@@ -7,11 +7,11 @@ log_path = "/mnt/log"
 
 @application.route("/")
 def hello():
-    with open(log_path) as f:
-        data = f.read()
+    with open(log_path, 'a+') as f:
+        f.write(socket.gethostname() + " " + str(ctime()) + "\n")
+	f.seek(0)
+	data = f.read()
     return data
 
 if __name__ == "__main__":
     application.run()
-    with open(log_path, "w") as f:
-        f.write(socket.gethostname() + " " + str(time()))
